d34d2033d96d8f4e87976ce1469cd7839dc7190f
Pheebes <phoebe.hardwicke@waterfield.com>
2015-08-11 15:15:11 -0500

    seed file is now generating sample skills for users.  Needs to be redone but works for now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
   users = User.create([{first_name: 'Alex', last_name: 'Hardins', years_of_experience: '15', email: 'fakeemail@gmail.com'}, {first_name: 'Bob', last_name: 'Smith', years_of_experience: '2', email: 'fake@gmail.com'}, {first_name: 'Cindy', last_name: 'Fletcher', years_of_experience: '6', email: 'email@gmail.com'}])
+  Skill.create(name: 'Javascript', level: 'beginner', user_id: 2)
+  Skill.create(name: 'HTML5', level: 'intermediate', user_id: 3)
+  Skill.create(name: 'CSS3', level: 'advanced', user_id: 1)
+  Skill.create(name: 'HTML5', level: 'advanced', user_id: 1)
+  Skill.create(name: 'CSS3', level: 'intermediate', user_id: 2)
