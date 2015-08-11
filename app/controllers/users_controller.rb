bdbde7e5947010ee5827ed25c34ac71802480d57
Pheebes <phoebe.hardwicke@waterfield.com>
2015-08-11 12:00:27 -0500

    setup update User via the api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :get_user, only: [:show, :update, :destroy]
 
+
   def get_user
     @user = User.find(params[:id])
   end
+
 
   def index
     respond_to do |format|
@@ -16,23 +18,21 @@ class UsersController < ApplicationController
     end
   end
 
+
   def show
     respond_to do |format|
-      format.html {
-
-      }
+      format.html {  }
       format.json {
         render json: @user.to_json
       }
     end
   end
 
+
   def create
     @user = User.new(user_params)
     respond_to do |format|
-      format.html {
-
-      }
+      format.html {   }
       format.json {
         if @user.save
           render status: 200, json: {
@@ -49,14 +49,35 @@ class UsersController < ApplicationController
     end
   end
 
+
   def update
+    respond_to do |format|
+       format.html {   }
+       format.json {
+        if @user.update(user_params)
+          render status: 200, json: {
+            message: "Successfully updated user!",
+            user: @user
+          }.to_json
+        else
+          render status: 422, json: {
+            message: "The user could not be updated.",
+            user: @user,
+            errors: @user.errors
+          }.to_json
+        end
+      }
+    end
   end
+
 
   def destroy
   end
 
+
   private
 
+  
   def user_params
     params.require(:user).permit(:first_name, :last_name, :years_of_experience, :email)
   end
